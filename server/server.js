117287510d2a9453b1442fd31456dc5388681e66
K_713n <94151881+yehlotien@users.noreply.github.com>
2026-04-20 02:01:03 +0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3001;
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 const model = genAI.getGenerativeModel({ 
-  model: "models/gemini-1.5-flash",
+  model: "gemini-1.5-flash-latest",
 }, { apiVersion: 'v1' }); // Thêm đoạn này để tránh lỗi v1beta
 
 app.use(cors({
